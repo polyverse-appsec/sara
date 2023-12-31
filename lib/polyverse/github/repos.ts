ff020ebf1d6df45dfc59984239a572bc1cc5616f
Alexander Gounares <alex@gounares.com>
2023-12-30 16:13:22 -0800

    fix getting private repos and fetch multiple pages as needed

--- lib/polyverse/github/repos.ts
+++ lib/polyverse/github/repos.ts
@@ -50,16 +50,34 @@ export async function fetchOrganizationRepositories({
   const octokit = new Octokit({
     auth: accessToken
   })
+  let page = 1
+  const repoNames = []
 
   try {
-    const response = await octokit.request('GET /orgs/' + org + '/repos', {
-      type: 'all',
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28'
+    while (true) {
+      const response = await octokit.request('GET /user/repos', {
+        visibility: 'all',
+        per_page: 100,
+        page: page,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      })
+
+      if (response.data.length === 0) {
+        break // Exit the loop if no more repos are returned
       }
-    })
-    //response has the repo array in the data field.  we just want the name
-    return response.data.map((repo: { name: any }) => repo.name)
+
+      repoNames.push(...response.data.map((repo: { name: any }) => repo.name))
+
+      console.log(
+        `Fetched page ${page}:`,
+        response.data.map((repo: { name: any }) => repo.name)
+      )
+      page++
+    }
+
+    return repoNames
   } catch (error) {
     console.error('Error fetching repositories:', error)
     throw error
