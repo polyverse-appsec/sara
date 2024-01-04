b3a3464d04716a384de4385f8c9dd1905bc41cc5
Chris Austin <caustin.adwan@gmail.com>
2024-01-04 14:40:43 -0800

    Query for chats using different indexing

--- app/actions.ts
+++ app/actions.ts
@@ -18,6 +18,8 @@ import { config } from 'process'
 import { Assistant } from 'openai/resources/beta/assistants/assistants'
 import { configAssistant } from '@/lib/polyverse/openai/assistants'
 
+const TEN_MINS_IN_MILLIS = 600000
+
 /**
  * Helper method that deletes properties from an object whose properties
  * evaluate to undefined. Note that it modifies the object in place.
@@ -47,30 +49,39 @@ export async function getChats(taskId?: string | null) {
 
   try {
     const pipeline = kv.pipeline()
+
     //if we have a taskId, get the chats associated with that task, otherwise get the chats associated with the user
-    const key = `task:chats:${taskId}`
-    console.log(`In getChats - key: ${key}`)
-    let chats: string[] = await kv.zrange(key, 0, -1, {
-      rev: true
-    })
+    const taskChatsKey = `task:chats:${taskId}`
+    const dateScoreInMillis = (Date.now() + TEN_MINS_IN_MILLIS)
+
+    // The original implementation of this based on
+    // https://github.com/vercel/ai-chatbot had a call of `zrange` with the
+    // `rev` option set to true and the indexes of (0, -1). For some reason
+    // this wouldn't return results for us. I believe that it may have to be
+    // with the score we assign which is millis since epoch. Something to look
+    // into in the future.
+    //
+    // Either way we don't reverse anymore and we query for all elements of the
+    // `task:chats:{taskId}` and we do a reverse of the results ourselves. This
+    // ought to return similar results as if we used the `rev` option with the
+    // indexes of (0, -1) since it is unlikely there will be collisions of the
+    // scores since we are in milliseconds.
+    //
+    // Note this depends on NTP being correctly configured on the host as well.
+    let chats: string[] = await kv.zrange(taskChatsKey, 0, dateScoreInMillis)
 
     if (!chats.length) {
-      //OMIGOD I CANT BELIEVE IM DOING THIS
-      chats = [
-        'chat:vDfCarx',
-        'chat:LypBqgk',
-        'chat:moLQ8zq',
-        'chat:irqtFq1',
-        'chat:WLatxmv'
-      ]
-      console.log('hackhack: forced load of chats')
+      return []
     }
+
+    chats.reverse()
+
     for (const chat of chats) {
       pipeline.hgetall(chat)
     }
 
     const results = await pipeline.exec()
-    console.log(`In getChats - results: ${JSON.stringify(results)}`)
+
     return results as Chat[]
   } catch (error) {
     console.error('Error getting chats: ', error)
