9254a44d94fd323b313dc41d74f6ab25b9544eba
Alexander Gounares <alex@gounares.com>
2024-01-04 23:24:19 -0800

    fix build break

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -3,6 +3,16 @@ import { Chat } from '@/components/chat'
 
 export default function IndexPage() {
   const id = nanoid()
+  //TODO what chat is this?  This is going to break us until we implement a 'user chat' with no
+  //repo selected.
+  const chat = {
+    id: id,
+    title: 'Chat',
+    userId: '1',
+    messages: [],
+    createdAt: new Date(),
+    path: `/chat/${id}`
+  }
 
-  return <Chat id={id} />
+  return <Chat chat={chat} />
 }
