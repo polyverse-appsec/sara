7bedfba4caf40f327fc01ccc88e28dae7c049e58
Chris Austin <caustin.adwan@gmail.com>
2024-01-24 19:35:04 -0800

    Add back in the page redirect in auth.ts to /sign-in

--- auth.ts
+++ auth.ts
@@ -52,7 +52,7 @@ export const {
       return !!auth?.user // this ensures there is a logged in user for -every- request
     }
   },
-  // pages: {
-  //   signIn: '/sign-in' // overrides the next-auth default signin page https://authjs.dev/guides/basics/pages
-  // }
+  pages: {
+    signIn: '/sign-in' // overrides the next-auth default signin page https://authjs.dev/guides/basics/pages
+  }
 })
