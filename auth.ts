32d9334096146710bcf2e76313dfa0a135ebb0bd
Chris Austin <caustin.adwan@gmail.com>
2024-01-25 08:24:08 -0800

    Add runtime env vars to Vercel deploy command for auth URLs

--- auth.ts
+++ auth.ts
@@ -5,10 +5,42 @@ import exp from 'constants'
 
 console.log(`***** auth.ts evaluation process.env.AUTH_REDIRECT_PROXY_URL 6: ${process.env.AUTH_REDIRECT_PROXY_URL}`)
 
+
+// TODO: Look at the NextAuth implementation and how they have
+// signIn(provider, options, authorizationParams) {
+//   return signIn(provider, options, authorizationParams, config)
+// },
+// signOut(options) {
+//   return signOut(options, config)
+// },
+// update(data) {
+//   return update(data, config)
+// },
+
+// const httpHandler = (req: NextRequest) => Auth(reqWithEnvUrl(req), config)
+
+
+// TODO: See these options for the auth config that we can pass when initializing
+//   useSecureCookies?: boolean
+// debug
+//   cookies?: Partial<CookiesOptions>
+// trustHost (seems scary)
+//   redirectProxyUrl?: string
+
+
 export const {
   handlers: { GET, POST },
   auth
 } = NextAuth({
+  debug: true,
+  cookies: {
+    callbackUrl: {
+      name: `__Secure-next-auth.callback-url`,
+      options: {
+        httpOnly: false, 
+      }
+    }
+  },
   // redirectProxyUrl: process.env.AUTH_REDIRECT_PROXY_URL,
   providers: [
     GitHub({
@@ -23,6 +55,7 @@ export const {
       console.log(`***** redirect call back baseUrl: ${baseUrl}`)
       console.log(`***** redirect call back process.env.NEXTAUTH_URL: ${process.env.NEXTAUTH_URL}`)
       console.log(`***** redirect call back process.env.AUTH_REDIRECT_PROXY_URL 6: ${process.env.AUTH_REDIRECT_PROXY_URL}`)
+      console.log(`***** redirect call back process.env.APP_ENV : ${process.env.APP_ENV}`)
 
       return url
     },
