219c2efcfba86e830094a2d4b7270984b7550997
Chris Austin <caustin.adwan@gmail.com>
2024-02-27 10:03:05 -0800

    Route away from `/projects` to `/orgs` if no billing org active

--- app/(projects)/projects/page.tsx
+++ app/(projects)/projects/page.tsx
@@ -1,8 +1,24 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
+import toast from 'react-hot-toast'
+
 import ProjectDashboard from './../../../components/prototypes/project-dashboard'
+import { useAppContext } from './../../../lib/hooks/app-context'
 
 const ProjectIndex = () => {
+  const router = useRouter()
+  const { activeBillingOrg } = useAppContext()
+
+  // Force a user to select an active billing org first before getting to their
+  // projects
+  if (!activeBillingOrg) {
+    toast.error(`Please select billing organization`)
+    router.push('/orgs')
+
+    return null
+  }
+
   return (
     <div className="flex-1 p-10 text-2xl font-bold">
       <ProjectDashboard />
