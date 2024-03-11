2d0e460da0855b3e35920646744c01ee30f50b3c
Chris Austin <caustin.adwan@gmail.com>
2024-03-11 06:34:53 -0700

    Disable theme toggle

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,11 +6,14 @@ import { SaraStatus } from './sara-status' // Ensure this is correctly imported
 import { ThemeToggle } from './theme-toggle' // Ensure this is correctly imported
 
 export const Footer = () => {
+  // 03/11/24: Commented out the theme toggle since the new UI components we
+  // are currently building don't have any theming CSS associated with them.
   return (
     <footer className="sticky bottom-0 z-50 w-full px-4 py-2 border-t bg-gradient-to-t from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
       <div className="flex items-center justify-between">
         <div className="flex items-center justify-start flex-1">
-          <ThemeToggle /> {/* Theme toggle on the left, left-justified */}
+          {/* <ThemeToggle />*/}
+          {/* Theme toggle on the left, left-justified */}
         </div>
         <div className="flex justify-center flex-1">
           <SaraStatus /> {/* SaraStatus centered in the middle */}
