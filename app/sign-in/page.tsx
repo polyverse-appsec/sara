a965f6d9139bdbc1aa4a15b6536a21f7db20e0c2
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2024-03-16 18:05:21 -0700

    tweak login page UI - shorter text, image render

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,4 +1,5 @@
 import { redirect } from 'next/navigation'
+import Image from 'next/image'
 
 import { auth } from './../../auth'
 import { LoginButton } from './../../components/login-button'
@@ -7,51 +8,38 @@ import SaraPortrait from './../../public/Sara_Cartoon_Portrait.png'
 
 export default async function SignInPage() {
   const session = await auth()
-  // redirect to home if user is already logged in
+  // Redirect to home if user is already logged in
   if (session?.user) {
     redirect('/')
   }
   return (
-    <div className="flex flex-col items-center justify-center p-4 ">
-      <img
-        src={SaraPortrait.src} // Adjust the path to your image
+    <div className="flex flex-col items-center justify-center p-4">
+      <Image
+        src={SaraPortrait}
         alt="Sara's Portrait"
-        width={200} // Adjust the width as needed
-        height={200} // Adjust the height as needed
+        width={200}
+        height={200}
       />
-      <p className="font-medium italic text-8xl">Sara</p>
+      <p className="font-medium italic text-8xl">Introducing Sara</p>
+      <p className="text-2xl text-gray-600">Your AI-powered Software Architect</p>
       <div className="flex items-center justify-center m-5">
         <p className="font-medium text-5xl mr-3">by</p>
-        <img
-          src={PolyverseLogo.src} // Adjust the path to your image
-          alt="Sara's Portrait"
-          width={200} // Adjust the width as needed
-          height={200} // Adjust the height as needed
+        <Image
+          src={PolyverseLogo}
+          alt="Polyverse Logo"
+          width={200}
+          height={200}
         />
       </div>
       <LoginButton />
-      <h2 className="text-xl text-blue-600 mt-8">How we are powered</h2>
-      <div className="w-1/2 border-t-2 border-blue-600 my-2"></div> 
-      <p className="mt-4 text-base text-center text-gray-700">
-          Boost from Polyverse is a set of AI-supercharged cloud services that
-          enables you to accelerate development, significantly improve
-          coordination across your organization, and deliver the high-quality
-          critical features to your customers. Boost analyzes and models your
-          software architecture, codebase, team specs and docs, development
-          workflows, and tasks... to learn how YOUR code works, how YOUR
-          organization delivers work, and how YOUR engineers can release
-          features faster.
-          <br />
-          <br />
-          Anyone in your organization can directly chat with Sara, your
-          personalized AI-supercharged Architect, to leverage the best of Boost
-          in real-time. Sara can build customized task plans based on your
-          personal or team or project goals. Finally, Sara will partner directly
-          with you and your team to deliver each task by writing the docs,
-          updating your architecture and specs, writing code, creating tests,
-          and even explaining complex technical requirements and algorithms to
-          team members and customers.
-        </p>
+      <h2 className="text-xl text-blue-600 mt-8">What Superpowers Sara</h2>
+      <div className="w-1/2 border-t-2 border-blue-600 my-2"></div>
+      <div className="mt-4 p-4 border-2 border-blue-600 rounded-lg text-base text-center text-gray-700">
+        Sara leverages advanced Polyvser Boost AI to analyze your codebase and workflows, providing real-time, customized task plans and direct assistance. Whether writing documentation, updating specs, coding, or creating tests, Sara partners with you to accelerate development and deliver high-quality features faster.
+
+        Sara learns about you and your team - your skills, strengths and challenges - to help you deliver your best work, and accelerate your roadmap and business goals.
+
+      </div>
     </div>
   )
 }
