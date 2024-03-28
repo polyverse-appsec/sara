d01a0fc45a48b95edb04e4b85c52b180210d6218
Chris Austin <caustin.adwan@gmail.com>
2024-03-28 08:10:28 -0700

    Add `DELETE /api/goals/<goalId>` REST endpoint

---
+++ app/api/authz.ts
@@ -0,0 +1,90 @@
+import Joi from 'joi'
+
+import { OrgPartDeux, ProjectPartDeux, UserPartDeux } from "./../../lib/data-model-types"
+
+
+interface SaraAuthZ {
+    orgListedOnUser: (user: UserPartDeux, orgId: string) => void
+    userListedOnOrg: (org: OrgPartDeux, userId: string) => void
+    userListedOnProject: (project: ProjectPartDeux, userId: string) => void
+}
+
+const orgListedOnUser = (user: UserPartDeux, orgId: string) => {
+    if (Joi.any().required().not(null).validate(user).error) {
+        throw new Error('User instance null/undefined')
+    }
+
+    if (Joi.any().required().not(null).validate(user.orgIds).error) {
+        throw new Error('User org IDs null/undefined')
+    }
+
+    if (Joi.number().greater(0).validate(user.orgIds.length).error) {
+        throw new Error('User org IDs empty')
+    }
+
+    if (Joi.string().required().validate(orgId).error) {
+        throw new Error('Org ID null/undefined/empty')
+    }
+
+    const foundOrgIdOnUser = user.orgIds.find((userOrgId) => userOrgId === orgId)
+
+    if (Joi.string().required().validate(foundOrgIdOnUser).error) {
+        throw new Error('Org ID not associated with user')
+    }
+}
+
+const userListedOnOrg = (org: OrgPartDeux, userId: string) => {
+    if (Joi.any().required().not(null).validate(org).error) {
+        throw new Error('Org instance null/undefined')
+    }
+
+    if (Joi.any().required().not(null).validate(org.userIds).error) {
+        throw new Error('Org user IDs null/undefined')
+    }
+
+    if (Joi.number().greater(0).validate(org.userIds.length).error) {
+        throw new Error('Org user IDs empty')
+    }
+
+    if (Joi.string().required().validate(userId).error) {
+        throw new Error('User ID null/undefined/empty')
+    }
+
+    const foundUserIdOnOrg = org.userIds.find((orgUserId) => orgUserId === userId)
+
+    if (Joi.string().required().validate(foundUserIdOnOrg).error) {
+        throw new Error('User ID not associated with org')
+    }
+}
+
+const userListedOnProject = (project: ProjectPartDeux, userId: string) => {
+    if (Joi.any().required().not(null).validate(project).error) {
+        throw new Error('Project instance null/undefined')
+    }
+
+    if (Joi.any().required().not(null).validate(project.userIds).error) {
+        throw new Error('Project user IDs null/undefined')
+    }
+
+    if (Joi.number().greater(0).validate(project.userIds.length).error) {
+        throw new Error('Project user IDs empty')
+    }
+
+    if (Joi.string().required().validate(userId).error) {
+        throw new Error('User ID null/undefined/empty')
+    }
+
+    const foundUserOnProject = project.userIds.find((projectUserId) => projectUserId === userId)
+
+    if (Joi.string().required().validate(foundUserOnProject).error) {
+        throw new Error('User ID not associated with project')
+    }
+}
+
+const authz: SaraAuthZ = {
+    orgListedOnUser,
+    userListedOnOrg,
+    userListedOnProject
+}
+
+export default authz
--- app/api/goals/[goalId]/route.ts
+++ app/api/goals/[goalId]/route.ts
@@ -6,11 +6,15 @@ import getGoal from './../../../../lib/polyverse/db/get-goal'
 import getOrg from './../../../../lib/polyverse/db/get-org'
 import getProject from './../../../../lib/polyverse/db/get-project'
 import getUser from './../../../../lib/polyverse/db/get-user'
+import deleteGoal from './../../../../lib/polyverse/db/delete-goal'
+import updateProject from './../../../../lib/polyverse/db/update-project'
+
+import authz from './../../../../app/api/authz'
 
 export const GET = auth(async (req: NextAuthRequest) => {
   const { auth } = req
 
-  if (!auth || !auth.accessToken || !auth.user.email) {
+  if (!auth || !auth.user.email) {
     return new Response(ReasonPhrases.UNAUTHORIZED, {
       status: StatusCodes.UNAUTHORIZED,
     })
@@ -90,10 +94,71 @@ export const GET = auth(async (req: NextAuthRequest) => {
     })
   } catch (error) {
     console.error(
-      `Failed fetching goal for '${auth.user.username}' because: ${error}`,
+      `Failed retrieving goal for '${auth.user.username}' because: ${error}`,
     )
 
-    return new Response('Failed to goal', {
+    return new Response('Failed to retrieve goal', {
+      status: StatusCodes.INTERNAL_SERVER_ERROR,
+    })
+  }
+}) as any
+
+export const DELETE = auth(async (req: NextAuthRequest) => {
+  const { auth } = req
+
+  if (!auth || !auth.user.email) {
+    return new Response(ReasonPhrases.UNAUTHORIZED, {
+      status: StatusCodes.UNAUTHORIZED,
+    })
+  }
+
+  try {
+    // TODO: Should be able to do Dynamic Route segments as documented:
+    // https://nextjs.org/docs/app/building-your-application/routing/route-handlers#dynamic-route-segments
+    // Problem is our Auth wrapper doesn't like it. See if we can figure
+    // out a way to move to this pattern.
+    const reqUrl = new URL(req.url)
+    const reqUrlSlices = reqUrl.toString().split('/')
+
+    // The last slice ought to be the slug for the goal ID
+    const requestedGoalId = reqUrlSlices[reqUrlSlices.length - 1]
+
+    const goal = await getGoal(requestedGoalId)
+    const org = await getOrg(goal.orgId)
+    const user = await getUser(auth.user.email)
+    const project = await getProject(goal.parentProjectId)
+
+    // Gross. I kind of hate having a try/catch in a try/catch but added this
+    // when first implementing our authZ helper. I can refactor this pattern
+    // after verifying the authZ helper works and use it everywhere.
+    try {
+      authz.userListedOnOrg(org, user.id)
+      authz.orgListedOnUser(user, org.id)
+      authz.userListedOnProject(project, user.id)
+    } catch (error) {
+      return new Response(ReasonPhrases.FORBIDDEN, {
+        status: StatusCodes.FORBIDDEN,
+      })
+    }
+
+    await deleteGoal(requestedGoalId)
+
+    // Be sure to update our project and remove the deleted goal ID from its
+    // list of associated goals
+    const goalIds = project.goalIds.filter((goalId) => goalId !== requestedGoalId)
+    project.goalIds = goalIds
+
+    await updateProject(project)
+
+    return new Response(ReasonPhrases.OK, {
+      status: StatusCodes.OK,
+    })
+  } catch (error) {
+    console.error(
+      `Failed deleting goal for '${auth.user.username}' because: ${error}`,
+    )
+
+    return new Response('Failed to delete goal', {
       status: StatusCodes.INTERNAL_SERVER_ERROR,
     })
   }
