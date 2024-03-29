a20219dd1d85c3a3da47e69c3dafefae1f6c3369
Chris Austin <caustin.adwan@gmail.com>
2024-03-29 06:08:26 -0700

    Use Radix UI for `<RenderableResource>` component

--- components/renderable-resource/renderable-resource.tsx
+++ components/renderable-resource/renderable-resource.tsx
@@ -1,11 +1,17 @@
 'use client'
 
+import { Flex } from '@radix-ui/themes'
+
 interface RenderableResourceProps {
   children: React.ReactNode
 }
 
 const RenderableResource = ({ children }: RenderableResourceProps) => {
-  return <div className="flex-1 flex-col p-10">{children}</div>
+  return (
+    <Flex direction="column" flexGrow="1" flexShrink="1" flexBasis="0%" p="5">
+      {children}
+    </Flex>
+  )
 }
 
 export default RenderableResource
