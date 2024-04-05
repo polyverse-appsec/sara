895f939fcd645617099f8e44a75988d31c276c3e
Chris Austin <caustin.adwan@gmail.com>
2024-04-05 06:52:57 -0700

    Add Storybook

---
+++ stories/components/sara-chat/sara-chat-query-content.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import SaraChatQueryContent from 'components/sara-chat/sara-chat-query-content'
+
+const meta = {
+    title: 'SaraChatQueryComponent',
+    component: SaraChatQueryContent,
+    parameters: {
+        layout: 'centered'
+    },
+    tags: ['autodocs']
+} satisfies Meta<typeof SaraChatQueryContent>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    args: {
+        content: 'Some content',
+        contentType: 'QUERY',
+        timestamp: new Date(),
+        shouldRenderLoadingSpinner: true,
+    }
+}
