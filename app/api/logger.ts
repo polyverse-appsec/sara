23de550bd9820a9ae5b447ce923d48dcaf462b5e
Chris Austin <caustin.adwan@gmail.com>
2024-04-09 12:02:18 -0700

    Add a logger class for backend

---
+++ app/api/logger.ts
@@ -0,0 +1,50 @@
+import pino from 'pino'
+
+import { Org, Project, User } from './../../lib/data-model-types'
+
+export interface SaraLogContext {
+    user?: User
+    org?: Org
+    project?: Project
+}
+
+const buildMergingObjectFromContext = (context: SaraLogContext) => ({ context: {
+    userId: context.user?.id,
+    userEmail: context.user?.email,
+    orgId: context.org?.id,
+    orgName: context.org?.name,
+    projectId: context.project?.id,
+    projectName: context.project?.name
+}})
+
+class SaraLogger {
+    logger: any
+
+    constructor() {
+        this.logger = pino()
+    }
+
+    error(message: string) {
+        this.logger.error(message)
+    }
+
+    errorWithContext(message: string, context: SaraLogContext) {
+        const mergingObject = buildMergingObjectFromContext(context)
+
+        this.logger.error(mergingObject, message)
+    }
+
+    info(message: string) {
+        this.logger.info(message)
+    }
+
+    infoWithContext(message: string, context: SaraLogContext) {
+        const mergingObject = buildMergingObjectFromContext(context)
+
+        this.logger.info(mergingObject, message)
+    }
+}
+
+const logger = new SaraLogger()
+
+export default logger
