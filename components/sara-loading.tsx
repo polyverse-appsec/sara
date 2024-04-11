3719f4f0215e4d697b3d2c7407bea3959012b58f
Chris Austin <caustin.adwan@gmail.com>
2024-04-11 11:39:30 -0700

    Update the Sara loading component with feedback

--- components/sara-loading.tsx
+++ components/sara-loading.tsx
@@ -8,25 +8,15 @@ import SaraLoadingImg from './../public/Sara-Loading.png'
 
 const SaraLoading = () => {
   return (
-    <Flex align="center" justify="center" minHeight="100vh">
-      <RenderableResourceContent>
-        <Flex direction="column" align="center">
-          <Flex>
-            <Image
-              src={SaraLoadingImg}
-              alt="Sara Loading"
-              title="Sara Loading"
-              width={200}
-              height={200}
-            />
-
-            <Flex direction="column" gapY="2">
-              <Heading>Hi, my name is Sara!</Heading>
-              <Text>I&apos;m preparing this content for you.</Text>
-            </Flex>
-          </Flex>
-        </Flex>
-      </RenderableResourceContent>
+    <Flex direction="column" align="center" justify="center" minHeight="100vh">
+      <Heading>I&apos;m preparing this content for you...</Heading>
+      <Image
+        src={SaraLoadingImg}
+        alt="Sara Loading"
+        title="Sara Loading"
+        width={700}
+        height={700}
+      />
     </Flex>
   )
 }
