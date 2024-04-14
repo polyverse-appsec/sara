06250cb75c2ec465e9d3e6290f120b72064af585
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2024-04-14 01:53:03 -0700

    enable emergency maintenance mode via env variable

--- app/(navigatable)/layout.tsx
+++ app/(navigatable)/layout.tsx
@@ -6,24 +6,32 @@ import { Flex, Theme } from '@radix-ui/themes'
 
 import HeaderCallouts from './../../components/callouts/header-callouts'
 import SidebarNav from './../../components/sidebar-nav'
+import { isPreviewFeatureEnabled } from 'lib/utils'
+import SaraLoading from 'components/sara-loading'
 
 interface NavigatableLayoutProps {
   children: React.ReactNode
 }
 
 const NavigatableLayout = ({ children }: NavigatableLayoutProps) => {
+  const isMaintenanceMode = isPreviewFeatureEnabled('EmergencyMaintenanceMode', '')
+
   return (
-    <>
-      <Theme>
-        <Flex direction="column" height="h-screen">
-          <HeaderCallouts />
-          <div className="flex flex-1 overflow-hidden">
-            <SidebarNav />
-            <div className="flex-1 overflow-auto">{children}</div>
-          </div>
-        </Flex>
-      </Theme>
-    </>
+    <Theme>
+      <Flex direction="column" height="h-screen">
+        <HeaderCallouts />
+        <div className="flex flex-1 overflow-hidden">
+          {isMaintenanceMode ? (
+            <SaraLoading message="Sara is currently undergoing maintenance. Please check back later." />
+          ) : (
+            <>
+              <SidebarNav />
+              <div className="flex-1 overflow-auto">{children}</div>
+            </>
+          )}
+        </div>
+      </Flex>
+    </Theme>
   )
 }
 
