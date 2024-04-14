9755a475181b0bb3597ebe56f569c5ef2088ef73
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2024-04-14 01:58:16 -0700

    enable support for email or domain specific emergency maintenance mode

--- app/(navigatable)/layout.tsx
+++ app/(navigatable)/layout.tsx
@@ -3,18 +3,35 @@
 import '@radix-ui/themes/styles.css'
 
 import { Flex, Theme } from '@radix-ui/themes'
+import { useEffect } from 'react'
 
 import HeaderCallouts from './../../components/callouts/header-callouts'
 import SidebarNav from './../../components/sidebar-nav'
 import { isPreviewFeatureEnabled } from 'lib/utils'
 import SaraLoading from 'components/sara-loading'
+import { SaraSession } from 'auth'
+import { useSession } from 'next-auth/react'
 
 interface NavigatableLayoutProps {
   children: React.ReactNode
 }
 
 const NavigatableLayout = ({ children }: NavigatableLayoutProps) => {
-  const isMaintenanceMode = isPreviewFeatureEnabled('EmergencyMaintenanceMode', '')
+  const session = useSession()
+  const saraSession = session.data ? (session.data as SaraSession) : null
+
+  useEffect(() => {
+
+    if (!saraSession) {
+        return
+    }
+  }, [saraSession])
+
+  if (!saraSession) {
+    return null
+  }
+
+  const isMaintenanceMode = isPreviewFeatureEnabled('EmergencyMaintenanceMode', saraSession.email)
 
   return (
     <Theme>
