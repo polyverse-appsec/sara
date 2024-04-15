0090691ed0bcb47c3fcc2e7695e3b0b8429bc211
Chris Austin <caustin.adwan@gmail.com>
2024-04-15 07:38:57 -0700

    Make about layout `use client`

--- app/(navigatable)/(about)/about/layout.tsx
+++ app/(navigatable)/(about)/about/layout.tsx
@@ -1,17 +1,18 @@
-import React, {ReactNode} from 'react';
-import { Flex } from '@radix-ui/themes';
+'use client'
+
+import React, { ReactNode } from 'react'
+import { Flex } from '@radix-ui/themes'
 
 interface LayoutProps {
-  children: ReactNode;
+  children: ReactNode
 }
+
 const Layout = ({ children }: LayoutProps) => (
   <Flex direction="column" gap="4">
     <div>
-      <main>
-        {children}
-      </main>
+      <main>{children}</main>
     </div>
   </Flex>
-);
+)
 
-export default Layout;
+export default Layout
