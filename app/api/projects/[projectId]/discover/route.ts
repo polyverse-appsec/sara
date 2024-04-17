b45a88eaf2805d35c40fe25e9d4de469fe6f8384
Chris Austin <caustin.adwan@gmail.com>
2024-04-17 05:35:18 -0700

    Fix build break by returning any instead of unknown

--- app/api/projects/[projectId]/discover/route.ts
+++ app/api/projects/[projectId]/discover/route.ts
@@ -58,4 +58,15 @@ export const POST = auth(async (req: NextAuthRequest) => {
       status: StatusCodes.INTERNAL_SERVER_ERROR,
     })
   }
-})
+}) as any
+
+// Note: Looks like based on this commit the NextAuth team are aware that their
+// wrapper is causing NextJS build errors:
+// https://github.com/nextauthjs/next-auth/commit/6a2f8a1d77c633ae3d0601a30f67523f38df2ecc
+//
+// Build errors would look something like:
+// app/api/orgs/route.ts
+// Type error: Route "app/api/orgs/route.ts" has an invalid export:
+//   "unknown" is not a valid GET return type:
+//     Expected "void | Response | Promise<void | Response>", got "unknown".
+
